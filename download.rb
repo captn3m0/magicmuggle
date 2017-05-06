86ec615a867eaf40d248907f9faf8fb537ccd2ac
Nemo <me@captnemo.in>
2017-05-06 14:44:39 +0530

    Kill BITLY with :fire:
    - It was too slow for any help

--- download.rb
+++ download.rb
@@ -2,10 +2,6 @@ require 'fileutils'
 require 'yaml'
 require 'bitly'
 
-Bitly.configure do |config|
-  config.api_version = 3
-  config.access_token = ENV['BITLY_KEY']
-end
 config = YAML.load(File.read 'contents.yml')
 
 for i, book in config
@@ -15,7 +11,7 @@ for i, book in config
     for link in book
         unless File.exists? "#{i}/#{chapter_index}.json"
             puts "Chapter #{chapter_index} downloading from #{link}"
-            json_path = Bitly.client.expand(link).long_url + '.json'
+            json_path = "https://www.reddit.com/r/magicmuggle/comments/#{link}/.json"
             `wget --no-clobber "#{json_path}" --output-document "#{i}/#{chapter_index}.json" -o /dev/null`
         end
         chapter_index += 1
